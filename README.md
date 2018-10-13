1b7d9d11c12ba5a38eab777a3da53a06314322ea
Scott Clarke <scott.clarke@student.manchester.ac.uk>
2018-10-13 14:36:05 +0100

    first version of script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # transactionSummary
+A script to take transactions downloaded from a bank, (specifically Santander, other banks may vary),
+in a .txt file and output the amount spent, the number of weeks covered by the transactions and
+the amount spent per week.
---
+++ statement.py
@@ -0,0 +1,18 @@
+#!/bin/python
+import fileinput
+from datetime import date
+
+def diff_dates(date1, date2):
+    return abs(date2-date1).days
+
+dates = ''
+for line in fileinput.input():
+  dates += line
+startDate = dates.split('\n')[0]
+endDate = dates.split('\n')[1]
+startDateSplit = startDate.split('/')
+endDateSplit = endDate.split('/')
+startDate = date(int(startDateSplit[2]), int(startDateSplit[1]),
+  int(startDateSplit[0]))
+endDate = date(int(endDateSplit[2]), int(endDateSplit[1]), int(endDateSplit[0]))
+print(diff_dates(startDate, endDate) / 7.0)
